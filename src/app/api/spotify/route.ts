35dbb0dac6e450a882b788b2a024168b5d3e8547
Ian Macabulos <iannmacabulos@gmail.com>
2026-04-19 23:30:41 +0800

    feat: Force Dynamic Execution

--- src/app/api/spotify/route.ts
+++ src/app/api/spotify/route.ts
@@ -1,6 +1,8 @@
 import { NextResponse } from "next/server";
 import { getNowPlaying } from "@/lib/spotify";
 
+export const dynamic = "force-dynamic";
+
 export async function GET() {
   try {
     const response = await getNowPlaying();
--- src/lib/spotify.ts
+++ src/lib/spotify.ts
@@ -30,7 +30,7 @@ export const getNowPlaying = async () => {
     headers: {
       Authorization: `Bearer ${access_token}`,
     },
-    // ISR: Cache the result and revalidate silently every 15 seconds
-    next: { revalidate: 30 },
+    // ISR: Cache the result and revalidate silently every 30 seconds
+    next: { revalidate: 30 }, 
   });
 };
